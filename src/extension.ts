c50e018d6daa082741cf4a0e6b8b5eef3c2b1373
Pachicobue <tigerssho@gmail.com>
2023-02-25 02:57:28 +0900

    ちょっとリファクタ

--- src/extension.ts
+++ src/extension.ts
@@ -1,47 +1,31 @@
 import * as vscode from 'vscode';
-import { basename, dirname } from 'path';
 import { glob } from 'glob';
-import { existsSync } from 'fs';
+import { basename, dirname } from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('oj-testcase-manager.pickTestcase', () => {
-        return pickTestcase();
-    });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('oj-testcase-manager.pickTestcase',
+        () => pickTestcase()
+    ));
 }
 
-async function pickTestcase() {
-    const editor = vscode.window.activeTextEditor;
-    if (editor === undefined) {
-        vscode.window.showErrorMessage('No active editor!');
-        return;
-    }
-    const language = editor.document.languageId;
-    if (language !== "cpp") {
-        vscode.window.showErrorMessage('C++ only supported!');
-        return;
-    }
-    const sourceFile = editor.document.fileName;
-    const sourceDir = dirname(sourceFile);
-    const basenameNoExt = basename(sourceFile, '.cpp');
-    const testcaseDir = `${sourceDir}/${basenameNoExt}_case`;
-    if (!existsSync(testcaseDir)) {
-        vscode.window.showErrorMessage(`${testcaseDir} not found!`);
-        return;
-    }
+async function getTestcaseList(testcaseDir: string): Promise<string[]> {
     return new Promise<string[]>((resolve, reject) => {
         glob('*.in', { cwd: testcaseDir }, (err, files) => {
-            if (err) {
-                vscode.window.showErrorMessage('Error during collecting testcase!');
-                return reject(err);
-            }
+            if (err) { return reject(err); }
             resolve(files);
         });
-    }).then((files) => {
-        return vscode.window.showQuickPick(files);
-    }).then((testCase) => {
-        return testCase;
     });
+}
+
+async function pickTestcase(): Promise<string | undefined> {
+    const editor = vscode.window.activeTextEditor;
+    if (editor === undefined || editor.document.languageId !== 'cpp') { return undefined; }
+    const sourceName = editor.document.fileName;
+    const basenameNoExt = basename(sourceName, '.cpp');
+    const dirName = dirname(sourceName);
+    const testcaseDir = `${dirName}/${basenameNoExt}_case`;
+    console.log(sourceName, basenameNoExt, dirName, testcaseDir);
+    return getTestcaseList(testcaseDir).then((files) => vscode.window.showQuickPick(files));
 }
 
 // This method is called when your extension is deactivated
