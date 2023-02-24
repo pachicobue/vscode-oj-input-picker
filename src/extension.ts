cf50f399fef5c9825e9872d536e4d04d2f8debfa
Pachicobue <tigerssho@gmail.com>
2023-02-24 15:57:01 +0900

    テストケース一覧取得

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { basename, dirname } from 'path';
+import { glob } from 'glob';
+import { existsSync } from 'fs';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('oj-testcase-manager.pickTestcase', () => {
+        return pickTestcase();
+    });
+    context.subscriptions.push(disposable);
+}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "oj-testcase-manager" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('oj-testcase-manager.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from oj-testcase-manager!');
-	});
-
-	context.subscriptions.push(disposable);
+async function pickTestcase() {
+    const editor = vscode.window.activeTextEditor;
+    if (editor === undefined) {
+        vscode.window.showErrorMessage('No active editor!');
+        return;
+    }
+    const language = editor.document.languageId;
+    if (language !== "cpp") {
+        vscode.window.showErrorMessage('C++ only supported!');
+        return;
+    }
+    const sourceFile = editor.document.fileName;
+    const sourceDir = dirname(sourceFile);
+    const basenameNoExt = basename(sourceFile, '.cpp');
+    const testcaseDir = `${sourceDir}/${basenameNoExt}_case`;
+    if (!existsSync(testcaseDir)) {
+        vscode.window.showErrorMessage(`${testcaseDir} not found!`);
+        return;
+    }
+    return new Promise<string[]>((resolve, reject) => {
+        glob('*.in', { cwd: testcaseDir }, (err, files) => {
+            if (err) {
+                vscode.window.showErrorMessage('Error during collecting testcase!');
+                return reject(err);
+            }
+            resolve(files);
+        });
+    }).then((files) => {
+        return vscode.window.showQuickPick(files);
+    }).then((testCase) => {
+        return testCase;
+    });
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
